6d6cc947e1cd4d86c6cd27490a2350d4f0fb1adf
Sherest Mehta <sherestdutt@gmail.com>
2024-06-14 13:17:59 +0530

    updated css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -86,6 +86,18 @@ async function loadFonts() {
   }
 }
 
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
 const tabElementMap = {};
 
 function calculateTabSectionCoordinate(main, lastTabBeginningIndex, targetTabSourceSection) {
